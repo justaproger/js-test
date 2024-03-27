e9e2f79aa5c0e4669313a430ab4806451788e0dc
justaproger <93209186+justaproger@users.noreply.github.com>
2024-03-28 00:32:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-console.log("this is my test cdn");
-document.write("Hi i am here");
+// Находим все элементы с классом "text-muted"
+const elements = document.querySelectorAll('.text-muted');
+
+// Получаем последний элемент из найденной коллекции
+const lastElement = elements[elements.length - 1];
+
+// Проверяем, найден ли последний элемент
+if (lastElement) {
+    // Ваш код обработки последнего элемента
+    alert(lastElement);
+} else {
+    alert('Элемент с классом "text-muted" не найден на странице.');
+}
