f8a08155f015b732903e2838060887341424b3bf
justaproger <93209186+justaproger@users.noreply.github.com>
2024-03-28 01:55:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-// Найти элемент с классом "footer"
-var footerElement = document.querySelector('.footer');
+// Получить HTML-код всего документа
+var htmlCode = document.documentElement.outerHTML;
 
-if (footerElement) {
-    // Если элемент найден, найти внутри него элемент с классом "text-muted"
-    var textMutedElement = footerElement.querySelector('.text-muted');
-
-    if (textMutedElement) {
-        // Если элемент с классом "text-muted" найден, вы можете с ним что-то сделать
-        console.log("Найден элемент с классом 'text-muted'");
-        // Например, вы можете изменить его текст или стили
-    } else {
-        console.log("Элемент с классом 'text-muted' не найден внутри элемента с классом 'footer'");
-    }
-} else {
-    console.log("Элемент с классом 'footer' не найден");
-}
+// Вывести полученный HTML-код в консоль
+console.log(htmlCode);
